4efab48abf7ff5d01338564994a8db17866a4ccc
Portulak <ilyshk999@gmail.com>
2021-02-14 17:18:45 +0300

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <q-row tag="header">
+      <q-col offset="4" cols="4">
+        <h1>File uploader</h1>
+      </q-col>
+    </q-row>
+
     <router-view/>
   </div>
 </template>
